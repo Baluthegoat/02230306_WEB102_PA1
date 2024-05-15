3d4a7dc4bb181939d4b946d8f312d5e40dd5306b
Baluthegoat <141105500+Baluthegoat@users.noreply.github.com>
2024-05-15 16:37:28 +0600

    wip

--- server.js
+++ server.js
@@ -5,21 +5,93 @@ const fs = require("fs");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
-  const path = reqUrl.pathname;
-  const method = req.method;
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const method = req.method.toUpperCase();
+
+  // Read data from JSON file
+  let todos = JSON.parse(fs.readFileSync("data.json", "utf8"));
 
   // Handle different routes and methods
-  if (path === "/todos" && method === "GET") {
-    // Read todos from JSON file and send as response
-    try {
-      const data = fs.readFileSync("data.json", "utf8");
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(data);
-    } catch (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
-    }
+  switch (path) {
+    case "/todos":
+      switch (method) {
+        case "GET":
+          // List all todos
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(todos));
+          break;
+        case "POST":
+          // Create a new todo
+          let body = "";
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+          req.on("end", () => {
+            const todo = JSON.parse(body);
+            todo.id = todos.length + 1; // Assigning a unique ID
+            todos.push(todo);
+            // Write data back to JSON file
+            fs.writeFileSync("data.json", JSON.stringify(todos), "utf8");
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(todo));
+          });
+          break;
+        default:
+          res.writeHead(405); // Method Not Allowed
+          res.end("Method Not Allowed");
+      }
+      break;
+    default:
+      // Handle /todos/:id route
+      if (path.startsWith("/todos/")) {
+        const todoId = parseInt(path.split("/")[2]);
+        const todo = todos.find((t) => t.id === todoId);
+
+        if (!todo) {
+          res.writeHead(404);
+          res.end("Todo not found");
+        } else {
+          switch (method) {
+            case "GET":
+              // Get a single todo by ID
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify(todo));
+              break;
+            case "PUT":
+              // Update a todo by ID
+              let body = "";
+              req.on("data", (chunk) => {
+                body += chunk.toString();
+              });
+              req.on("end", () => {
+                const updatedTodo = JSON.parse(body);
+                Object.assign(todo, updatedTodo);
+                // Write data back to JSON file
+                fs.writeFileSync("data.json", JSON.stringify(todos), "utf8");
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(JSON.stringify(todo));
+              });
+              break;
+            case "DELETE":
+              // Delete a todo by ID
+              const index = todos.indexOf(todo);
+              todos.splice(index, 1);
+              // Write data back to JSON file
+              fs.writeFileSync("data.json", JSON.stringify(todos), "utf8");
+              res.writeHead(204);
+              res.end();
+              break;
+            default:
+              res.writeHead(405); // Method Not Allowed
+              res.end("Method Not Allowed");
+          }
+        }
+      } else {
+        res.writeHead(404);
+        res.end("Not Found");
+      }
+      break;
   }
 });
 
